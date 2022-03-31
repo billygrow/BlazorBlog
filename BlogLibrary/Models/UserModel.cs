c467edef94a67088faa4c785c69074e86b60d9e8
Billy Grow <williamegrow@gmail.com>
2022-03-30 22:24:25 -0500

    added models

---
+++ BlogLibrary/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLibrary.Models;
+public class UserModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string ObjectIdentifier { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    public string EmailAddress { get; set; }
+    public List<SimplePostModel> AuthoredPosts { get; set; } = new();
+    public List<SimplePostModel> CommentedOnPosts { get; set; } = new();
+
+}
